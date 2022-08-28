6805854aa9eb36719022c1e729ba35b7362d4527
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-08-28 14:39:28 -0300

    feat: adicionando api de geração de jwt dinamica

---
+++ api/index.js
@@ -0,0 +1,35 @@
+import Express from "express";
+import bodyParser from "body-parser";
+import jwt from 'jsonwebtoken';
+import { totp } from 'otplib';
+import { jwtByPayload } from "./data/fakePayload.js";
+
+const app = Express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Em um ambiente real recomenda-se que a "secret" seja um HASH criptografado e armazenado com segurança no .env
+const secret = "KVKFKRCPNZQUYMLXOVYDSQKJKZDTSRLD";
+
+app.get('/generate-dynamic-jwt', (request, response, next) => {
+    console.log(`********** GenerateDynamicUrl **********`);
+    try {
+        const decoded = jwt.verify(jwtByPayload, secret);
+        const newSecret = Buffer.from(`${decoded.id}`).toString('base64');
+
+        const time = totp.timeRemaining();
+        const securityToken = totp.generate(newSecret);
+        decoded.totpToken = securityToken;
+        const newJwt = jwt.sign(decoded, secret);
+
+        response.json({ time: time, newJwt: newJwt })
+    } catch (error) {
+        console.log(`Generate Dynamica Jwt Error ${error}`);
+        next(error);
+    }
+})
+
+app.listen(8080, () => {
+    console.log('Listening in 8080')
+})
